f490c15634fd342ba9e031f751833b3b7b82095e
wangjinxing01 <254231654@qq.com>
2020-02-02 15:37:28 +0800

    add files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.pattern.*;
+import com.pattern.TempleteMethod.*;
 import com.util.*;
 
 import java.util.ArrayList;
@@ -9,6 +10,23 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        testSingleTon();
+        testTempleteMethod();
+    }
+
+    private static void testTempleteMethod() {
+        ProjectManager icsdProjMgr = new IcsdProjectManager();
+        ProjectManager segProjMgr = new SegProjectManager();
+        icsdProjMgr.doTask();
+        icsdProjMgr.chooseMembers();
+        icsdProjMgr.report();
+
+        segProjMgr.doTask();
+        segProjMgr.chooseMembers();
+        segProjMgr.report();
+    }
+
+    private static void testSingleTon() {
         ArrayList<MultiThread> multiThreads = new ArrayList<>(20);
         for (int i = 0; i < 20; i++) {
             multiThreads.add(new MultiThread("com.pattern.SingleTon", "getInstance"));
